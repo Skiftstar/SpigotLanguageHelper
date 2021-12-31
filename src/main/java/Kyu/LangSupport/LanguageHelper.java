01dfaf862033331ca4ebf06dbee1d2f1fc76b410
Yukami <46356129+Skiftstar@users.noreply.github.com>
2021-12-31 23:04:22 +0100

    added DB support and default game Language Support

--- src/main/java/Kyu/LangSupport/LanguageHelper.java
+++ src/main/java/Kyu/LangSupport/LanguageHelper.java
@@ -7,7 +7,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 public final class LanguageHelper {
@@ -17,23 +23,31 @@ public final class LanguageHelper {
 
     private String prefix;
 
+    private boolean useDB;
+    private DB database;
+
     private YamlConfiguration pLangConf;
     private File pLangFile;
 
     private Map<String, Map<String, String>> messages = new HashMap<>();
     private Map<String, Map<String, List<String>>> lores = new HashMap<>();
 
-    private Map<Player, String> playerLangs = new HashMap<>();
+    private Map<UUID, String> playerLangs = new HashMap<>();
 
     private JavaPlugin plugin;
 
-    public LanguageHelper(JavaPlugin plugin, String defaultLang, Reader langResource, String prefix) {
+    public LanguageHelper(JavaPlugin plugin, String defaultLang, Reader langResource, String prefix, boolean useDB) {
         this.plugin = plugin;
+        this.useDB = useDB;
         this.defaultLang = defaultLang;
         this.defaultLangResource = langResource;
         this.prefix = prefix;
 
         setup();
+    }
+
+    public void setDatabase(DB database) {
+        this.database = database;
     }
 
     public void setup() {
@@ -54,7 +68,8 @@ public final class LanguageHelper {
         }
 
         loadMessages();
-        new MessageJoinListener(plugin, this);
+        MessageJoinListener listener = new MessageJoinListener(plugin, this);
+        plugin.getServer().getMessenger().registerIncomingPluginChannel(plugin, "my:channel", listener);
     }
 
     private void loadMessages() {
@@ -114,39 +129,43 @@ public final class LanguageHelper {
         saveConfig(defaultConf, file);
     }
 
-
     public List<String> getLore(Player p, String loreKey) {
         String pLang;
-        if (!playerLangs.containsKey(p)) {
+        if (!playerLangs.containsKey(p.getUniqueId())) {
             pLang = defaultLang;
             setupPlayer(p);
         } else {
-            pLang = playerLangs.get(p);
+            pLang = playerLangs.get(p.getUniqueId());
         }
         if (!lores.containsKey(pLang)) {
-            return lores.get(defaultLang).getOrDefault(loreKey, new ArrayList<>(Arrays.asList(color("&cLore &4 " + loreKey + " &c not found!"))));
+            return lores.get(defaultLang).getOrDefault(loreKey,
+                    new ArrayList<>(Arrays.asList(color("&cLore &4 " + loreKey + " &c not found!"))));
         } else {
-            return lores.get(pLang).getOrDefault(loreKey, new ArrayList<>(Arrays.asList(color("&cLore &4 " + loreKey + " &c not found!"))));
+            return lores.get(pLang).getOrDefault(loreKey,
+                    new ArrayList<>(Arrays.asList(color("&cLore &4 " + loreKey + " &c not found!"))));
         }
     }
 
     public List<String> getLore(String loreKey) {
-        return lores.get(defaultLang).getOrDefault(loreKey, new ArrayList<>(Arrays.asList(color("&cLore &4 " + loreKey + " &c not found!"))));
+        return lores.get(defaultLang).getOrDefault(loreKey,
+                new ArrayList<>(Arrays.asList(color("&cLore &4 " + loreKey + " &c not found!"))));
     }
 
     public String getMess(Player p, String messageKey, boolean... usePrefix) {
         String pLang;
-        if (!playerLangs.containsKey(p)) {
+        if (!playerLangs.containsKey(p.getUniqueId())) {
             pLang = defaultLang;
             setupPlayer(p);
         } else {
-            pLang = playerLangs.get(p);
+            pLang = playerLangs.get(p.getUniqueId());
         }
         String message;
         if (!messages.containsKey(pLang)) {
-            message = messages.get(defaultLang).getOrDefault(messageKey, color("&cMessage &4" + messageKey + "&c not found!"));
+            message = messages.get(defaultLang).getOrDefault(messageKey,
+                    color("&cMessage &4" + messageKey + "&c not found!"));
         } else {
-            message = messages.get(pLang).getOrDefault(messageKey, color("&cMessage &4" + messageKey + "&c not found!"));
+            message = messages.get(pLang).getOrDefault(messageKey,
+                    color("&cMessage &4" + messageKey + "&c not found!"));
         }
         if (usePrefix.length > 0 && usePrefix[0]) {
             message = prefix + message;
@@ -155,7 +174,8 @@ public final class LanguageHelper {
     }
 
     public String getMess(String messageKey, boolean... usePrefix) {
-        String message = messages.get(defaultLang).getOrDefault(messageKey, color("&cMessage &4" + messageKey + "&c not found!"));
+        String message = messages.get(defaultLang).getOrDefault(messageKey,
+                color("&cMessage &4" + messageKey + "&c not found!"));
         if (usePrefix.length > 0 && usePrefix[0]) {
             message = prefix + message;
         }
@@ -163,15 +183,77 @@ public final class LanguageHelper {
     }
 
     public void setupPlayer(Player p) {
-        if (pLangConf.get(p.getUniqueId().toString()) == null) {
-            p.sendMessage(getMess("NoLangSet", true).replace("%default", defaultLang));
-            pLangConf.set(p.getUniqueId().toString(), defaultLang);
-            saveConfig(pLangConf, pLangFile);
-            playerLangs.put(p, defaultLang);
+        if (!isUseDB()) {
+            if (pLangConf.get(p.getUniqueId().toString()) == null) {
+                String gameLanguage = getLanguage(p);
+                String defaultLang = this.defaultLang;
+                if (messages.get(gameLanguage) != null) {
+                    defaultLang = gameLanguage;
+                }
+
+                pLangConf.set(p.getUniqueId().toString(), defaultLang);
+                saveConfig(pLangConf, pLangFile);
+                playerLangs.put(p.getUniqueId(), defaultLang);
+                p.sendMessage(getMess(p, "NoLangSet", true).replace("%default", defaultLang));
+            } else {
+                String lang = pLangConf.getString(p.getUniqueId().toString());
+                playerLangs.put(p.getUniqueId(), lang);
+            }
         } else {
-            String lang = pLangConf.getString(p.getUniqueId().toString());
-            playerLangs.put(p, lang);
+            Connection conn = database.getConnection();
+            try (PreparedStatement stmt = conn.prepareStatement("SELECT lang FROM langusers WHERE uuid = ?;")) {
+                stmt.setString(1, p.getUniqueId().toString());
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.next()) {
+                    String lang = rs.getString("lang");
+                    playerLangs.put(p.getUniqueId(), lang);
+                } else {
+                    String gameLanguage = getLanguage(p);
+                    String defaultLang = this.defaultLang;
+                    if (messages.get(gameLanguage) != null) {
+                        defaultLang = gameLanguage;
+                    }
+
+                    PreparedStatement statemt = conn.prepareStatement("INSERT INTO langusers(uuid, lang) VALUES(?, ?);");
+                    statemt.setString(1, p.getUniqueId().toString());
+                    statemt.setString(2, defaultLang);
+                    statemt.execute();
+                    statemt.close();
+
+                    pLangConf.set(p.getUniqueId().toString(), defaultLang);
+                    saveConfig(pLangConf, pLangFile);
+                    playerLangs.put(p.getUniqueId(), defaultLang);
+                    p.sendMessage(getMess(p, "NoLangSet", true).replace("%default", defaultLang));
+                }
+                conn.close();
+                stmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    public String getLanguage(Player p) {
+        String language = null;
+        try {
+            Object ep = getMethod("getHandle", p.getClass()).invoke(p, (Object[]) null);
+            Field f = ep.getClass().getDeclaredField("locale");
+            f.setAccessible(true);
+            language = (String) f.get(ep);
+            language = language.split("_")[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return language;
+    }
+
+    private Method getMethod(String name, Class<?> clazz) {
+        for (Method m : clazz.getDeclaredMethods()) {
+            if (m.getName().equals(name))
+                return m;
+        }
+        return null;
     }
 
     private String color(String s) {
@@ -186,19 +268,39 @@ public final class LanguageHelper {
         }
     }
 
-
-    public void changeLang(Player p, String newLang) {
+    public void changeLang(UUID p, String newLang) {
         playerLangs.remove(p);
         playerLangs.put(p, newLang);
-        pLangConf.set(p.getUniqueId().toString(), newLang);
+        pLangConf.set(p.toString(), newLang);
         saveConfig(pLangConf, pLangFile);
+
+        if (isUseDB()) {
+            Connection conn = database.getConnection();
+            try (PreparedStatement stmt = conn.prepareStatement("UPDATE langusers SET lang = ? WHERE uuid = ?;")) {
+                stmt.setString(1, newLang);
+                stmt.setString(2, p.toString());
+                stmt.executeUpdate();
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void remPlayer(Player p) {
-        playerLangs.remove(p);
+        playerLangs.remove(p.getUniqueId());
     }
 
     public String getDefaultLang() {
         return defaultLang;
+    }
+
+    public boolean isUseDB() {
+        return useDB;
+    }
+
+    public DB getDatabase() {
+        return database;
     }
 }
