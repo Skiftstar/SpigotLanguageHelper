a6ebfcb7a50ae6f4dba7cda6f9f45c2600d2527a
Yukami <46356129+Skiftstar@users.noreply.github.com>
2023-02-22 13:02:47 +0100

    test

--- src/main/java/Kyu/LangSupport/DB/MariaDB.java
+++ src/main/java/Kyu/LangSupport/DB/MariaDB.java
@@ -69,6 +69,7 @@ public class MariaDB implements DB {
         PreparedStatement stmt = null;
         for (String query : queries) {
             // If you use the legacy way you have to check for empty queries here.
+            helperInstance.getPlugin().getLogger().info("QUERY + " + query);
             if (query.isBlank())
                 continue;
             stmt = conn.prepareStatement(query);
